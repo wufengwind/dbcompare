fe2b57f5e80ab0f55e5293070f0b92c8026e017f
wufengwind <464243312@qq.com>
2025-06-05 20:07:48 +0800

    版本信息格式调整

--- src/main/java/com/dbcompare/util/VersionInfo.java
+++ src/main/java/com/dbcompare/util/VersionInfo.java
@@ -96,6 +96,6 @@ public class VersionInfo {
         System.out.println("软件作者: " + AUTHOR);
         System.out.println("哈希签名: " + gitCommit);
         System.out.println("构建时间: " + buildTime);
-        System.out.println("***************************************************");
+        System.out.println("******************************************************");
     }
 } 
